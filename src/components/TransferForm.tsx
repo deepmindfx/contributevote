5a101ed05fe703a8c064de3fe36e96194e8b93dd
deepmindfx <deepmindfx01@gmail.com>
2025-05-10 22:01:07 +0100

    refactor: use sonner for toast notifications in TransferForm

--- src/components/TransferForm.tsx
+++ src/components/TransferForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-hot-toast';
+import { toast } from 'sonner';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
